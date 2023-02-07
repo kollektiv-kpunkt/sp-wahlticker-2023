be94b3d48c60f65d91efc93e4405a073f550e5df
timothyoesch <54284649+timothyoesch@users.noreply.github.com>
2023-02-07 22:17:56 +0100

    Add and Remove Kandi Watching functions

--- webpage/app/Models/PoliticianResult.php
+++ webpage/app/Models/PoliticianResult.php
@@ -33,4 +33,29 @@ class PoliticianResult extends Model
     {
         return $this->belongsTo(Party::class, "partyId", "partyId");
     }
+
+    public function constituency()
+    {
+        return $this->belongsTo(Constituency::class, "constituencyId", "id");
+    }
+
+    public function addChatInterested($chatId) {
+        if (in_array($chatId, $this->chats_interested ?? [])) {
+            return false;
+        } else {
+            $this->chats_interested = array_merge($this->chats_interested ?? [], [$chatId]);
+            $this->save();
+            return true;
+        }
+    }
+
+    public function removeChatInterested($chatId) {
+        if (in_array($chatId, $this->chats_interested ?? [])) {
+            $this->chats_interested = array_diff($this->chats_interested ?? [], [$chatId]);
+            $this->save();
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
